4f58cbdf7cea5fbe4d025826d7826c31e89c5561
Janus Varmarken <varmarken@gmail.com>
2018-10-25 19:47:52 -0700

    Cleanup: remove binaries; remove Maruf's unused serialization code.

--- playground.c
+++ playground.c
@@ -1,42 +1,12 @@
 #include <stdio.h>
-//#include <mpi.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include "msgshm238.h"
 #include <string.h>
 
-void strings() {
-    char str[] = "no notion of 'wrapper' String class/struct like in java;\nstrings are declared as char arrays.\n";
-    printf("%s", str);
-}
+// Playground for exercising the library.
 
 int main(void) {
-
-//    char * str1 = "string1";
-//    char * str2 = "string2";
-//    send(str1, 42);
-//    send(str2, 42);
-
-    // Note: attempts to read messages sent by self
-//    msg* msg1 = recv(42);
-//    printf("read message: { senderId=%d; rcvrId=%d; payload='%s'; }\n", msg1->senderId, msg1->rcvrId, msg1->payload);
-//    msg* msg2 = recv(42);
-//    printf("read message: { senderId=%d; rcvrId=%d; payload='%s'; }\n", msg2->senderId, msg2->rcvrId, msg2->payload);
-    // There should be no more messages left in buffer at this point.
-    // Make sure we handle this corner case.
-//    msg* msg3 = recv(42);
-//    if (NULL == msg3) {
-//        printf("buffer empty, recv() returned NULL\n");
-//    } else {
-//        printf("read message: { senderId=%d; rcvrId=%d; payload='%s'; }\n", msg3->senderId, msg3->rcvrId, msg3->payload);
-//    }
-
-
-//    char * str1 = "string1";
-//    char * str2 = "string2";
-
-//    char * str = malloc(sizeof(*str));
-
     printf("PID: %d Running\n",getpid());
     printf("enter s to send and r to receive : ");
     char ch;
@@ -66,8 +36,6 @@ int main(void) {
                 }
             }
             send(str, receiver_id);
-
-
         }
     }
     else if(ch == 'r'){
@@ -83,8 +51,5 @@ int main(void) {
 
         }
     }
-
     return 0;
 }
-
-
